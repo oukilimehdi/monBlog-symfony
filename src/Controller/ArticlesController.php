9e9c307adef12ec79ff607563b88c5f16b973cb7
mehdi <oukili.mehdi@gmail.com>
2021-11-08 12:04:53 +0100

    update and remove article ok *

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,8 +37,61 @@ class ArticlesController extends AbstractController
              'article' => $article
             
         ]);
+    }
+
+   /**
+    * @Route("update/{id}", name="articles_update")
+    */
+    public function update($id, ArticleRepository $articleRepo, Request $request)
+    {
+        $article = $articleRepo->findOneBy(["id" => $id]);
+        if(!$article){
+            return $this->redirectToRoute('articles');
+        }
+
+        $user = $this->getUser();
+        $article = $articleRepo->findOneBy(["user" => $user->getId()]);
+        $form = $this->createForm(ArticleFormType::class, $article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = new Article;
+            $article = $form->getData();
+            $user = $this->getUser();
+            $article->setUser($user);
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('user');
+        }
+
+        return $this->render('user/modifierArticle.html.twig', [
+            'form' => $form->createView(),
+            'article' => $article
+        ]);
 
     }
+   /**
+    * @Route("delete/{id}", name="articles_delete")
+    */
+    public function delete($id, ArticleRepository $articleRepo, Request $request)
+    {
+       
+        $article = $articleRepo->findOneBy(["id" => $id]);
+        if(!$article){
+            return $this->redirectToRoute('articles');
+        }
+
+        $em= $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
+        $this->addFlash('articleSuprime', "votre article a bien été suprimé");
+
+        return $this->redirectToRoute('user');
+
+    }
+
+
+
 
 }
 
